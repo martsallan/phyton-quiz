8056c46f3a533f37672187fbfe0af5ebd49282f7
Allan Martins <allanmartins@id.uff.br>
2021-01-25 19:38:56 -0300

    desafio: add meta tags

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import QuizLogo from '../src/components/QuizLogo'
 import QuizBackground from '../src/components/QuizBackground'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
+import Head from 'next/head'
+import Link from 'next/link'
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -26,28 +28,36 @@ export const QuizContainer = styled.div`
 
 export default function Home() {
   return (
-    <QuizBackground backgroundImage={db.bg}>
-      <QuizContainer>
-        <QuizLogo />
-        <Widget>
-          <Widget.Header>
-            <h1>{db.title}</h1>
-          </Widget.Header>
-          <Widget.Content>
-            <p>{db.description}</p>
-          </Widget.Content>
-        </Widget>
+    <>
+      <Head>
+        <title>Phyton Quiz</title>
+        <meta property="og:title" content="Phyton Quiz" key="title" />
+        <meta property="og:image" content={db.bg}/>
+        <meta property="og:image:type" content="image/jpg"/>
+      </Head>   
+      <QuizBackground backgroundImage={db.bg}>
+        <QuizContainer>
+          <QuizLogo />
+          <Widget>
+            <Widget.Header>
+              <h1>{db.title}</h1>
+            </Widget.Header>
+            <Widget.Content>
+              <p>{db.description}</p>
+            </Widget.Content>
+          </Widget>
 
-        <Widget>
-          <Widget.Content>
-            <h1>Quizes da Galera</h1>
+          <Widget>
+            <Widget.Content>
+              <h1>Quizes da Galera</h1>
 
-            <p>lorem ipsum dolor sit amet...</p>
-          </Widget.Content>
-        </Widget>
-        <Footer />
-      </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/martsallan" />
-    </QuizBackground>
+              <p>lorem ipsum dolor sit amet...</p>
+            </Widget.Content>
+          </Widget>
+          <Footer />
+        </QuizContainer>
+        <GitHubCorner projectUrl="https://github.com/martsallan" />
+      </QuizBackground>
+    </>
   );
 }
