70f217129d8fcb6d1600f928d9b2df57fc048f34
Allan Martins <allanmartins@id.uff.br>
2021-01-27 15:05:19 -0300

    day 3 (input, quiz page)

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import db from '../db.json';
@@ -8,24 +7,9 @@ import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
-
-// const BackgroundImage = styled.div`
-//   background-image: url(${db.bg});
-//   flex: 1;
-//   background-size: cover;
-//   background-position: center;
-// `;
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
+import QuizContainer from '../src/components/QuizContainer';
 
 export default function Home() {
   const router = useRouter();
@@ -38,6 +22,7 @@ export default function Home() {
         <meta property="og:title" content="Phyton Quiz" key="title" />
         <meta property="og:image" content={db.bg} />
         <meta property="og:image:type" content="image/jpg" />
+        <link rel="icon" type="image/png" href="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/768px-Python-logo-notext.svg.png" />
       </Head>
       <QuizBackground backgroundImage={db.bg}>
         <QuizContainer>
@@ -48,25 +33,23 @@ export default function Home() {
             </Widget.Header>
             <Widget.Content>
               <p>{db.description}</p>
-              <form onSubmit={function (infosDoEvento) {
+              <form onSubmit={(infosDoEvento) => {
                 infosDoEvento.preventDefault();
-                router.push(`/quiz?name=${name}`);
                 // router
+                router.push(`/quiz?name=${name}`);
               }}
               >
-                <input
-                  onChange={function (infosDoEvento) {
-                    // State
-                    // name = infosDoEvento.target.value;
+                <Input
+                  name="nomeDoUsuario"
+                  onChange={(infosDoEvento) => {
                     setName(infosDoEvento.target.value);
                   }}
                   placeholder="Diz aí seu nome para jogar :)"
+                  value={name}
                 />
-                <button type="submit" disabled={name.length === 0}>
-                  Jogar
-                  {' '}
-                  {name}
-                </button>
+                <Button type="submit" disabled={name.length === 0}>
+                  {`Jogar ${name}`}
+                </Button>
               </form>
             </Widget.Content>
           </Widget>
--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Widget = styled.div`
   margin-top: 24px;
   margin-bottom: 24px;
-  border: 1px solid ${({ theme }) => theme.colors.primary};
+  border: 1px solid ${({ theme }) => theme.colors.secondaryDarker};
   background-color: ${({ theme }) => theme.colors.mainBg};
   border-radius: 4px;
   overflow: hidden;
@@ -43,6 +43,23 @@ Widget.Content = styled.div`
   ul {
     list-style: none;
     padding: 0;
+  }
+`;
+
+Widget.Topic = styled.a`
+  outline: 0;
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.contrastText};
+  background-color: ${({ theme }) => `${theme.colors.primary}40`};
+  padding: 10px 15px;
+  margin-bottom: 8px;
+  cursor: pointer;
+  border-radius: ${({ theme }) => theme.borderRadius};
+  transition: .3s;
+  display: block;
+  &:hover,
+  &:focus {
+    opacity: .5;
   }
 `;
 
